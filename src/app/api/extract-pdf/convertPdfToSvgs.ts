e90e411f210595d8f337ac7f747600a82f72c9ed
Romilly Eveleigh <romillysemail@gmail.com>
2024-10-29 13:07:35 +0000

    add error logs

--- src/app/api/extract-pdf/convertPdfToSvgs.ts
+++ src/app/api/extract-pdf/convertPdfToSvgs.ts
@@ -7,12 +7,17 @@ export const convertPdfToSvgs = async (pdfBuffer: Buffer) => {
 
   pdfjs.GlobalWorkerOptions.workerSrc = await 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.0.279/pdf.worker.min.js';
 
-  // Convert PDF to images with optimized settings
+  try {
+    // Convert PDF to images with optimized settings
   const imageBuffers = await pdf2img.convert(pdfBuffer, {
     base64: false,
     scale: 2,
     width: 1500, // Set a fixed width for consistency and potential speed improvement
   });
 
-  return imageBuffers
+    return imageBuffers;
+  } catch (error) {
+    console.error("Error during PDF to SVG conversion:", error);
+    throw error;
+  }
 };
