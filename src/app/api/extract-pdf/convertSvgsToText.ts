bf431b92f78a8b06098b0d29e7e5ea384d3f9cba
Romilly Eveleigh <romillysemail@gmail.com>
2024-10-31 12:00:38 +0000

    extend maxDuration tp 60

--- src/app/api/extract-pdf/convertSvgsToText.ts
+++ src/app/api/extract-pdf/convertSvgsToText.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 30;
+export const maxDuration = 60;
 
 import { createWorker, createScheduler, PSM } from "tesseract.js";
 
--- src/app/api/extract-pdf/fallbackOcrTextExtraction.ts
+++ src/app/api/extract-pdf/fallbackOcrTextExtraction.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 30;
+export const maxDuration = 60;
 
 
 import { convertPdfToSvgs } from "./convertPdfToSvgs";
