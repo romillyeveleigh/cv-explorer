8f548c09240725b23a08bfcca26c10d84fd09dae
Romilly Eveleigh <romillysemail@gmail.com>
2024-10-31 12:49:21 +0000

    change resolution to 1500

--- src/app/api/extract-pdf/convertPdfToSvgs.ts
+++ src/app/api/extract-pdf/convertPdfToSvgs.ts
@@ -12,7 +12,7 @@ export const convertPdfToSvgs = async (pdfBuffer: Buffer) => {
     const imageBuffers = await pdf2img.convert(pdfBuffer, {
       base64: false,
       scale: 2,
-      width: 1000, // Set a fixed width for consistency and potential speed improvement
+      width: 1500, // Set a fixed width for consistency and potential speed improvement
     });
 
     let timeEnd = Date.now();
