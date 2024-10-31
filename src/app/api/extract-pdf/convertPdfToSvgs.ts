7223e40fe1786e8d02d8fd842d0b7d07167e7c96
Romilly Eveleigh <romillysemail@gmail.com>
2024-10-31 12:37:09 +0000

    changed width to 1000

--- src/app/api/extract-pdf/convertPdfToSvgs.ts
+++ src/app/api/extract-pdf/convertPdfToSvgs.ts
@@ -1,31 +1,18 @@
 "use server";
 
-// import path2d
-// import { Path2D } from "path2d";
-// console.log("🚀 ~ Path2D:", Path2D)
-
-// @ts-ignore
-// import * as pdfworker from "pdfjs-dist/legacy/build/pdf.worker.mjs";
-// console.log("🚀 ~ pdfworker:", pdfworker)
-
-
 export const convertPdfToSvgs = async (pdfBuffer: Buffer) => {
   let timeStart = Date.now();
   try {
     // @ts-ignore
     await import("pdfjs-dist/legacy/build/pdf.worker.mjs");
-
     await import("path2d");
-    // make it global
-    // globalThis.Path2D = Path2D as any;
-
-    // console.log("🚀 ~ Path2D:", Path2D)
     const pdf2img = await import("pdf-img-convert");
+
     // Convert PDF to images with optimized settings
     const imageBuffers = await pdf2img.convert(pdfBuffer, {
       base64: false,
       scale: 2,
-      width: 1500, // Set a fixed width for consistency and potential speed improvement
+      width: 1000, // Set a fixed width for consistency and potential speed improvement
     });
 
     let timeEnd = Date.now();
