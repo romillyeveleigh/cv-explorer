9c977f442ae8cbe0d535b061352473733afb98d0
Romilly Eveleigh <romillysemail@gmail.com>
2024-11-07 15:43:30 +0000

    trim next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,8 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: [
       "tesseract.js",
-      "canvas",
-      "pdfjs-dist",
-      "pdfjs-dist/legacy/build/pdf.worker.min.js",
-      "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
-      "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
       "pdfjs-dist/legacy/build/pdf.worker.mjs",
       "path2d",
-      // "pdf-parse",
-      // "fs",
     ],
     outputFileTracingIncludes: {
       "/api/**/*": ["./node_modules/**/*.wasm", "./node_modules/**/*.proto"],
